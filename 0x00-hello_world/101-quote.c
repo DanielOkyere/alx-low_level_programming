96ac5623b288e8a55a228afe16a7b4c4c821c8e6
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-04-07 20:04:23 +0000

    8 completed and fixed all betty formatting

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 int main(void)
 {
 int t = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", t);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", t);
 return (1);
 }
