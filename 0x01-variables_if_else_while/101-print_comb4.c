95ea05e4da1e4259b2e7449b07b23fff483b0661
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-04-08 20:48:56 +0000

    done 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Print all possible different combinations of 3 digits
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 1000; i++)
+	{
+		j = i / 100;
+		k = (i / 10) % 10;
+		l = i % 10;
+
+		if (j < k && k < l)
+		{
+			putchar(j + '0');
+			putchar(k + '0');
+			putchar(l + '0');
+
+			if (i < 700)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
