4c78cb08f7d377725abc413841d392eef9e3eae0
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-04-08 13:58:18 +0000

    fixed betty indentations

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,13 +17,13 @@ int main(void)
 	a = n % 10;
 	if (a > 5)
 	{
-	  printf("Last digit of %i is %i and is greater than 5\n", n, a);
+		printf("Last digit of %i is %i and is greater than 5\n", n, a);
 	} else if (a == 0)
 	{
-	  printf("Last digit of %i is %i and is 0\n", n, a);
+		printf("Last digit of %i is %i and is 0\n", n, a);
 	} else
 	{
-	  printf("Last digit of %i is %i and is less than 6 and not 0\n", n, a);
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, a);
 	}
 	return (0);
 }
