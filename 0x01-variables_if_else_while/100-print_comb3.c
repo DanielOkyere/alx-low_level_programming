fd22cb73bba1fce58d5f8f06e3ede57e71aca409
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-04-08 19:25:40 +0000

    fix 100 print combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Advanced question 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			if (i != j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(',');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
