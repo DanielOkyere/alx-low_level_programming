a1ae131a70713f0ac660c1cce1186d982c2e4b63
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-04-08 12:44:20 +0000

    complete 1st assignment

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+puts("is positive\n");
+}
+if (n == 0)
+{
+puts("is zero\n");
+}
+if (n < 0)
+{
+puts("is negative\n");
+}
+return (0);
+}
