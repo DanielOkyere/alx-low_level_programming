150d1b9d5b7c5c046b7be38b90327ac44a9a701b
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-04-12 15:55:00 +0000

    3 is slower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - _islower function
+ * Description: Checks if character is lowercase
+ * Return: 1 if char is lowerchase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
