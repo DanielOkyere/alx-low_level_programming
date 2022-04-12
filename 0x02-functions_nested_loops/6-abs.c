4c200ff7a8ffade3e2d5d5c750f28bfc1939bc92
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-04-12 16:29:28 +0000

    5 - sign c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - _abs function
+ * @n: n to provide absolute value
+ * Description: Provides Absolute value of number
+ * Return: int
+ */
+
+int print_sign(int n)
+{
+	return ((n < 0) ? (n * (-1)) : (n));
+}
