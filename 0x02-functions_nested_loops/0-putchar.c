82f0e514ee9bb18407a2fa999823dd03e91bc5fc
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-04-12 15:26:05 +0000

    fixed 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,14 @@
 
 int main(void)
 {
-	char printPuchar[] = "_putchar";
-	int i;
-
-	for (i = 0; i <= 8; i++)
-	{
-		_putchar(printPuchar[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
