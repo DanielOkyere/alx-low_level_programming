4555305d9ebc78c5777c4e58d3b64ad9d2c04285
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-04-12 15:33:57 +0000

    1 alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints_alphabet
+ * Description: Prints all alphabets
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
