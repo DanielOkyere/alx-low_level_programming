67851ccfc7eec2e20113ef2647f496b19c485cc8
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-04-14 19:39:11 +0000

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - print_square
+ * @size: size of square
+ * Description: prints a square of number
+ * Return: void
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	y = 0;
+
+	if (size < 1)
+		_putchar('\n');
+
+	while (y < size)
+	{
+		x = 0;
+		while (x < size)
+		{
+			_putchar('#');
+			x++;
+		}
+		_putchar('\n');
+		y++;
+	}
+}
--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -1,10 +1,8 @@
 #include "main.h"
 int main(void)
 {
-	print_line(0);
-	print_line(2);
-	print_line(10);
-	print_line(10);
-	print_line(-4);
+	print_square(2);
+	print_square(10);
+	print_square(0);
 	return (0);
 }
