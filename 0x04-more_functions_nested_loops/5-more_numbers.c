d6c6b0f7f66d0706c6c4882a397ea83ae22dbce2
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-04-14 16:57:07 +0000

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,19 +2,21 @@
 
 /**
  * more_numbers - more_numbers function
- * Description: Prints numbers from 0 to 9
+ * Description: Prints numbers from 0 to 14, 10 times
  * Return: void
  */
 
 void more_numbers(void)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i <= 10; i++)
+	for (j = 0; j <= 9; j++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			_putchar(i + '0');
+			if (i > 9)
+				_putchar((i / 10) + '0');
+			_putchar(i % 10 + '0');
 		}
 		_putchar('\n');
 	}
