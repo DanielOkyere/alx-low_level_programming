b2706ae51acbb82da62ff18c59b60ec90ebc39ab
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-04-15 05:13:17 +0000

    fixed largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,18 +12,6 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && a > c)
-	{
-		largest = a;
-	}
-	if (b >= a && b > c)
-	{
-		largest = b;
-	}
-	else
-	{
-		largest = c;
-	}
-
+	largest = a > b ? (a > c ? a : c) : (b > c ? b : c);
 	return (largest);
 }
