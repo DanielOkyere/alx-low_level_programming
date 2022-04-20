d9f3a3fa89bffe746e2e8646479a1e93f88c450c
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-04-20 23:18:38 +0000

    6 puts complete

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - print one char out of 2 fo a string
+ * @str: char array string type
+ * Descrption: Print 1st char, then 3, then 5th
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] !='\0'; i++)
+	{
+		if (i % 2 == 0)
+			putchar(str[i]);
+	}
+	putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -2,10 +2,9 @@
 #include <stdio.h>
 int main(void)
 {
-	char s[10] = "My School";
+	char *str;
 
-	printf("%s\n", s);
-	rev_string(s);
-	printf("%s\n", s);
+	str = "0123456789";
+	puts2(str);
 	return (0);
 }
