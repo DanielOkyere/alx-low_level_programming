c10102d877832e781eddc156b23d96aeb5613a5f
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-04-20 21:10:37 +0000

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_string/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset to 98
+ * @n: pointer to n value
+ * Description: resets to 98
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	int *nptr = n;
+	*nptr = 98;
+}
