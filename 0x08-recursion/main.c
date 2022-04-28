d61f1b171292cbc1740945cd9f8a4de050a9bab9
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-04-28 19:00:00 +0000

    recursion print rev recursion

--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-	_puts_recursion("Puts with recursion");
+	_print_rev_recursion("\nColton Walker");
 	return (0);
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void _print_rev_recursion(char *s);
 void _puts_recursion(char *s);
 int _putchar(char c);
 #endif
