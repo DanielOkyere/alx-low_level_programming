9003ecf73f66b4bf84adc03db15cdb0dbe37962c
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-05-04 22:20:49 +0000

    nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: argc
+ * @argv: argv
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	int u = argc - 1;
+	(void)argv;
+
+	printf("%d\n", u);
+
+	return (0);
+}
