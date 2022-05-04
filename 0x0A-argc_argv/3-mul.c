34a36c8547ae12c5dfaf522d38f3c4e039195f49
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-05-04 22:49:07 +0000

    fixing 3-mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,9 +9,9 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc > 3 || argc < 2)
+	if (argc != 3)
 	{
-		fprintf(stderr, "Error\n");
+		printf("%s\n ","Error");
 		return (1);
 	}
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
