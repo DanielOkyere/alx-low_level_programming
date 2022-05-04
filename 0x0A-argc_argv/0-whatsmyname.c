17c9eb04be427843ba8b78c603a36642aa9c96ec
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-05-04 22:07:51 +0000

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main function prints arg[0]
+ * @argc: argc
+ * @argv: Array
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = argc - argc;
+
+	printf("%s\n", argv[i]);
+	return(0);
+}
