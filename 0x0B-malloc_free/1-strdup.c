1a21875627a528c5448394d5e5fca919f66c034d
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-05-05 11:26:15 +0000

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+int _strlen(char *c);
+
+/**
+ * _strdup - creates a copy
+ * @str: string to be copied
+ * Return: char pointer
+ */
+
+char *_strdup(char *str)
+{
+	char *sptr;
+	int i = 0;
+
+	sptr = malloc((sizeof(char) * _strlen(str)) + 1);
+	if (sptr == NULL)
+		return (NULL);
+	while (i < _strlen(str))
+	{
+		sptr[i] = str[i];
+		i++;
+	}
+
+	return (sptr);
+}
+
+/**
+ * _strlen - checks stringlength
+ * @c: char
+ * Return: int
+ *
+ */
+int _strlen(char *c)
+{
+	char *p = c;
+
+	while (*p != '\0')
+		p++;
+
+	return (p - c);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,5 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-
+char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
 #endif
