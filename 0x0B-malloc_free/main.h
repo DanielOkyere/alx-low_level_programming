a500378d6d1eb685cad0801c001f19dd19831e13
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-05-05 19:21:37 +0000

    4 - free grid

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+void free_grid(int **grid, int height);
 int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
