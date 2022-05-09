3033e2df16ad0e5cede9caaf5bffac2e68c6d1b5
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-05-09 20:50:39 +0000

    101-mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,8 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+void _print(char *str, int l);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int *array_range(int min, int max);
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
