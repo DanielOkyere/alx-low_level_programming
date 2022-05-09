826b57de905a2d92740c74ec101195d3ee6bafcc
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-05-09 20:14:48 +0000

    fixses

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - create an array of integers inclusive of min
+ * @min: min value to include
+ * @max: max value to include
+ * Return: pointer to newly created array
+ */
+
+int *array_range(int min, int max)
+{
+	int *p, i;
+
+	if (min > max)
+		return (NULL);
+	p = malloc((max - min + 1) * sizeof(*p));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; min <= max; i++, min++)
+		p[i] = min;
+	return (p);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+int *array_range(int min, int max);
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int _strlen(char *s);
