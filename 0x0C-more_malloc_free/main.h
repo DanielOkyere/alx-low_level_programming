df17ff9de694d7268d14afa8af7f0e3cbc9eb0a0
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-05-10 09:03:53 +0000

    fixing 5

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,11 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+
+void *mul_array(char *a1, int len1, char a2, char *a3, int lena);
+void print_array(char *a, int nb);
+int _atoi_digit(char x);
+int _isNumber(char *argv);
 char *mul(char n, char *num, int num_index, char *dest, int dest_index);
 int check_for_digits(char **av);
 void init(char *str, int l);
