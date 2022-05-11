4c2a2ae2282fdc0e5549847cf35abe3d4d98bc55
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-05-11 16:05:26 +0000

    fixes function like

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
-#ifndef MACRO_H
-#define MACRO_H
+#ifndef LIKEMACRO_H
+#define LIKEMACRO_H
 #endif
 
 #ifndef ABS
-#define ABS(x) ((x < 0) ? (x * (-1)) : (x))
+#define ABS(x) ((x < 0) ? (x) * -(1) : (x))
 #endif
