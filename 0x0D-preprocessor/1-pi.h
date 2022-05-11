1426af13217679c041420b5ce929b8e34cce3df6
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-05-11 11:06:39 +0000

    1-PI

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#endif
+
+#ifndef PI
+typedef int make_iso_compilers_happy;
+#define PI 3.14159265359
+#endif
--- 0x0D-preprocessor/main.c
+++ 0x0D-preprocessor/main.c
@@ -1,16 +1,19 @@
-#include "0-object_like_macro.h"
-#include "0-object_like_macro.h"
+#include "1-pi.h"
+#include "1-pi.h"
 #include <stdio.h>
 
 /**
  * main - check the code
- * Return: Always 0
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	int s;
+    float a;
+    float r;
 
-	s = 98 + SIZE;
-	printf("%d\n", s);
-	return (0);
+    r = 98;
+    a = PI * r * r;
+    printf("%.3f\n", a);
+    return (0);
 }
