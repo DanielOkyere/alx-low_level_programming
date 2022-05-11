1d2da86f9702ecbe7851a0c77d1a0baf42df5d4e
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-05-11 14:11:51 +0000

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - creates a struct of dog
+ * @name: name of dog(char *)
+ * @age: age of dog(float)
+ * @owner: owner of dog(char *)
+ * Description: creates a structure of dogs
+ */
+typedef struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+} dog_t;
+#endif
