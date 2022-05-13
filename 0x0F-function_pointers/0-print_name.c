58dfee59e5210eba8594289609e4587f1f0cf767
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-05-13 10:59:58 +0000

    0-print_name.c done

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - prints name
+ * @name: pointer to text
+ * @f: function pointer
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+		name = "";
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,5 @@
+#ifndef _FUNCTION_POINTER_H
+#define _FUNCTION_POINTER_H
+
+void print_name(char *name, void (*f)(char *));
+#endif
