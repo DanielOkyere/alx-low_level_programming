855c8f52666613f49dd06524bd2aaa8581201921
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-05-13 19:02:42 +0000

    fixed 1 arrey iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,7 +9,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	long unsigned int i;
+	size_t i;
+
+	if (array == NULL || action == NULL)
+		return;
 
 	for (i = 0; i < size; i++)
 	{
