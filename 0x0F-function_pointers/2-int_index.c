4ca40190b8cda56101d0d22438229265c2126f00
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-05-13 19:06:00 +0000

    fixing errors

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,7 +11,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size < 0 || array == NULL)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
@@ -19,6 +19,4 @@ int int_index(int *array, int size, int (*cmp)(int))
 		if (cmp(array[i]) > 0)
 			return (i);
 	}
-
-	return (-1);
 }
