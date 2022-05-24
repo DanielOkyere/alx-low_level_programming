e6390d511b457b00db639eeb6fa2b908bd9c80b0
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-05-24 22:51:16 +0000

    fixed 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
 /**
  * before_main - Print statement within before running  main
  */
-void __attribute((constructor)) before_main(void)
+void __attribute__((constructor)) before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
