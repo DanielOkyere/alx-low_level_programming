2a06e5c4441e0f9dfe93d9e99057a034b9f6f9e1
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-05-24 22:32:41 +0000

    4-free_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -17,6 +17,7 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+void free_list(list_t *head);
 list_t *add_node_end(list_t **head, const char *str);
 list_t *add_node(list_t **head, const char *str);
 size_t print_list(const list_t *h);
