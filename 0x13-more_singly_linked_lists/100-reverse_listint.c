666b36b3e29d43fb76bc7c31287a0abf73874a27
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-05-26 21:35:23 +0000

    100 && 101 print

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -6,7 +6,7 @@
  * Description: Limited to going through loop only once.
  * Not allowed to use `malloc` or `free` or arrays
  * Can only declare a max of 2 variables.
- * Return: pointer to first node of 
+ * Return: pointer to first node
  */
 listint_t *reverse_listint(listint_t **head)
 {
--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,7 +5,7 @@
  * print_listint_safe - Print a `listint_t` linked list including mem addresses
  * @head: head of linked list
  * Description: Go through the list only once.
- * Return: number of nodes in list. If fails, exit with status 98.
+ * Return: number of nodes in list. If fails, exit with status 98
  */
 size_t print_listint_safe(const listint_t *head)
 {
