8adec69198f6ef96cf4249b5509000098d3b4f5c
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-05-26 21:32:29 +0000

    101 print safe

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -16,6 +16,7 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+size_t print_listint_safe(const listint_t *head);
 listint_t *reverse_listint(listint_t **head);
 listint_t *find_listint_loop(listint_t *head);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
