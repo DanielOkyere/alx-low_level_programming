4897067cd05e6d55cf9def6e157b54698fec961d
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-05-28 03:00:38 +0000

    fixing segmentation fault on linked list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,7 +9,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *hptr = malloc(sizeof(listint_t));
 
-	if (hptr == NULL && !n)
+	if (hptr == NULL)
 		return (NULL);
 
 	hptr->n = n;
