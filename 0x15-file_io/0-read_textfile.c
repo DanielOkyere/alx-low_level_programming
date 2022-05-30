0508571caaba17e6c01eb467af208893886e4447
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-05-30 22:26:53 +0000

    fixing 0-read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,28 +13,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t rd, wd;
 	char *buffer;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
-	fd = open(filename, O_RDWR);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		free(buffer);
-		return (0);
-	}
-	rd = read(fd, buffer, letters);
-	if (rd == -1)
+	if (!buffer)
 		return (0);
 
+	rd = read(fd, buffer, letters);
 	wd = write(STDOUT_FILENO, buffer, rd);
 
-	if (wd == -1 || rd != wd)
-		return (0);
-
-	free(buffer);
 	close(fd);
+	free(buffer);
 
 	return (wd);
 }
