86ca7e6b86f4b9bcb31f9556f0a9d78f67958883
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-05-30 22:29:58 +0000

    fixing 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,6 +15,8 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
+	if (!text_content)
+		text_content = "";
 	n = write(fd, text_content, strlen(text_content));
 	if (n == -1)
 		return (-1);
