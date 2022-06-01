67b2d931b87cee443cef497a530660e0c250aae0
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-06-01 21:42:06 +0000

    message

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 #include <stddef.h>
+int clear_bit(unsigned long int *n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 int _putchar(char s);
