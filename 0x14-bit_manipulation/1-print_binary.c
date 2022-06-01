e4688bd364d0e40ce6947d359a9c49fa8f439200
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-06-01 15:15:19 +0000

    print_binary done

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_binary - prints binary from number
+ * @n: long int
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+
+	_putchar((n & 1) + '0');
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 #include <stddef.h>
+int _putchar(char a);
+void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 #endif
