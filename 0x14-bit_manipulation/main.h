06b17a833ee8ac4f08847cb6ecef7002236e6ecf
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-06-01 15:46:31 +0000

    1 print_binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 #include <stddef.h>
-int _putchar(char a);
+int _putchar(char s);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 #endif
