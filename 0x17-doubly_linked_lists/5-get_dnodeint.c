e30df03e900ac7a81d7f149570dc15a3bbcdda3d
Daniel Okyere <daniel.kwame.okyere101@gmail.com>
2022-07-07 16:19:36 +0000

    fixes for get_dnode

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,6 +12,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *hptr = head;
 	unsigned int count = 0;
 
+	if (head == NULL)
+		return (NULL);
 	while (hptr->next != NULL)
 	{
 		if (count == index)
