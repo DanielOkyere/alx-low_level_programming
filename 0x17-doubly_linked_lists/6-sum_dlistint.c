d354c809cf51f7296c88eab7c34d55512c8ff81e
Daniel Okyere <daniel.kwame.okyere101@gmail.com>
2022-07-07 17:02:27 +0000

    6 sum dlistint

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -10,7 +10,7 @@ int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
 
-	if (head == NULL || head->next == 0)
+	if (head == NULL || head->next == NULL)
 		return (0);
 
 	while (head->next != NULL)
