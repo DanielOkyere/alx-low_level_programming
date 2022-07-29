91932f60946a3d895300c81c75a72092e221f1b0
Daniel Okyere <daniel.kwame.okyere101@gmail.com>
2022-07-29 05:24:00 +0000

    initial commit hash talble

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table with value submitted
+ * @size: unsigned long int the size of the table
+ * Return: hash_table_t* pointer to the hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ptr = malloc(size * sizeof(hash_node_t));
+
+	return (ptr);
+}
