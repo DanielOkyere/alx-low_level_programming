4a3ae48416e41e5724ff37c0cd8b53dd34de8dd6
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-09-14 11:52:24 +0000

    Complete 0 - 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+5-island_perimeter.py
+"""
+def island_perimeter(grid):
+    """
+    Determines the perimeter of island
+    0- represents water zone
+    1- represents a land zone
+    One cell is a square with side 1
+    Grid cells are connected horizontally/vertically(not diagonally)
+    Grid rectangular width and height dont exceed 100
